fed607f95ff2006a809cb1ad0e5990513211a86a
koogiyun <gyu523@naver.com>
2020-06-18 18:38:48 +0900

    pc버전 완료.

---
+++ cloneCoding2/app.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", () => {
+	const menuBtn = document.querySelector(".header__menu-btn");
+	const slideMenu = document.querySelector(".slide-menu");
+	const menuLink = document.querySelector(".menu__link");
+	const menuCloseBtn = document.querySelector(".menu__close-btn");
+	const videoEl = document.querySelector(".banner__video");
+
+	menuBtn.addEventListener("click", () => {
+		slideMenu.classList.toggle("active");
+	});
+	menuCloseBtn.addEventListener("click", () => {
+		slideMenu.classList.toggle("active");
+	});
+	menuLink.addEventListener("click", () => {
+		slideMenu.classList.toggle("active");
+	});
+	document.addEventListener("click", e => {
+		if (e.target.className.match("menu")) {
+			return;
+		}
+		slideMenu.classList.remove("active");
+	});
+});
